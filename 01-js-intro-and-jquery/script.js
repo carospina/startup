61ca26731214e55bedafc42e54c5e3235718a18a
carospina <spinacarolina@gmail.com>
2014-06-09 20:44:08 -0300

    Update Topic 1

--- 01-js-intro-and-jquery/script.js
+++ 01-js-intro-and-jquery/script.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
-  $('section.hidden').fadeIn(400, function() {
-    $('input.alias').focus();
+  var $section = $('section.hidden'),
+      $button = $($section[0]).find('button'),
+      $input = $($section[0]).find('input.alias'),
+      $response = $($section[0]).find('.response');
+  $section.fadeIn(400, function() {
+    $input.focus();
   });
-  $('button').click(function() {
-    var name = $('input.alias').val();
+  $button.click(function() {
+    var name = $input.val();
     $.get('http://bootcamp.aws.af.cm/welcome/'+name, function(data) {
-      $('.response')
+      $response
         .html(highlight(name,data.response));
     })
       .fail(function(data) {
-        $('.response')
+        $response
           .addClass("error")
           .text('ERROR');
       });
@@ -18,11 +22,12 @@ $(document).ready(function() {
 });
 
 function highlight(name,data) {
-  var nameHigh = '<span style="background-color: #FFFF00">'+name+'</span>';
+  var nameHigh = '<span class="highlight">'+name+'</span>';
   return data.replace(name,nameHigh);
 }
 
 function getTweets() {
+  var $tweets = $('section.tweets');
   $.getJSON('http://tweetproxy.ap01.aws.af.cm/search?callback=?',{q: "html5"}, function(data) {
     console.log(data);
     $.each(data.statuses,function(i,tweet) {
@@ -35,7 +40,7 @@ function getTweets() {
                     + '<p>'+text+'</p>'
                     + '<footer>'+created+'</footer>'
                     + '</article>';
-      $('section.tweets').append(art_tweet);
+      $tweets.append(art_tweet);
 
     })
   })
